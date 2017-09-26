856e0441e3546918bbc5af3ae4883c4423787c3f
Carl Page <crlpage7@gmail.com>
2017-09-26 15:25:39 -0500

    created dev and dist folders

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp');
     imagemin = require('gulp-imagemin'),
     stripDebug = require('gulp-strip-debug'),
     uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
     // concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     plumber = require('gulp-plumber'),
@@ -19,54 +20,63 @@ var plumberErrorHandler = {
   })
 };
 
+// Delete the dist directory
+gulp.task('clean', function() {
+  return gulp.src('./dev/**')
+    .pipe(clean());
+});
+
+// sass compilation and css minification
 gulp.task('styles', function() {
-  gulp.src('./scss/main.scss')
+  gulp.src('./dev/scss/main.scss')
     .pipe(plumber(plumberErrorHandler))
     .pipe(sass())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(autoprefixer())
-    .pipe(cleanCSS({level: 2}))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(cleanCSS({ level: 2 }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
+// js file minification
 gulp.task('scripts', function() {
-  gulp.src(['./scripts/**.js', '!./js/*/min.js'])
+  gulp.src(['./dev/scripts/**.js', '!./js/*/min.js'])
     .pipe(plumber(plumberErrorHandler))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(stripDebug())
-    .pipe(uglify().on('error', function(err) {
-      gutil.log(gutil.colors.red('[Error]'), err.toString());
-      this.emit('end');
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(uglify().on('error', function(err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); this.emit('end'); }))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-// gulp.task('images', function() {
-//   gulp.src('./images/*.*')
-//     .pipe(plumber(plumberErrorHandler))
-//     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-//     .pipe(gulp.dest('./img'))
-//     .pipe(notify({ message: 'Images task complete' }));
-// });
+// image compression task
+gulp.task('images', function() {
+  gulp.src('./dev/images/*')
+    .pipe(plumber(plumberErrorHandler))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('./dist/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
 
+// html reload
 gulp.task('html', function() {
   gulp.src(['./**.html'])
     .pipe(plumber(plumberErrorHandler))
     .pipe(browserSync.reload({stream: true}));
 });
 
+// watch all tasks
 gulp.task('serve', function() {
   browserSync.init({
     server: {
       baseDir: './'
     }
   });
-  gulp.watch('./scripts/*.js', ['scripts']);
-  gulp.watch('./scss/*.scss', ['styles']);
-  // gulp.watch('./images/*.*', ['images']);
-  gulp.watch('./*.html', ['html']);
+  gulp.watch('./dev/scripts/*.js', ['scripts']);
+  gulp.watch('./dev/scss/*.scss', ['styles']);
+  gulp.watch('./dev/images/*.*', ['images']);
+  gulp.watch('./dev/*.html', ['html']);
 });
 
-gulp.task('default', ['styles', 'html', 'scripts', 'serve']);
+gulp.task('default', ['styles', 'html', 'scripts', 'images', 'serve']);
